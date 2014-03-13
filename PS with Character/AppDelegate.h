a4452f45d73f6a5631a1e3762849fb546b47d899
samuraipapa <yury.gitman@gmail.com>
2014-03-13 14:30:43 -0400

    Initial Commit

---
+++ PS with Character/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PS with Character
+//
+//  Created by yg on 3/13/14.
+//  Copyright (c) 2014 YuryGitman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PS with Character/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  PS with Character
+//
+
+//  Copyright (c) 2014 YuryGitman. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
